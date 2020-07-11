bd697ccc67e471a445331d014ec286e16c8b57e2
thienthao <37302688+thienthao@users.noreply.github.com>
2020-07-12 02:53:23 +0700

    API validation

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
@@ -100,6 +101,11 @@ namespace API.Controllers
         [HttpPost("register")]
         public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
         {
+            if (CheckEmailExistAsync(registerDto.Email).Result.Value)
+            {
+                return new BadRequestObjectResult(new ApiValidationErrorResponse { Errors = new[] { "Email address is in use" } });
+            }
+
             var user = new AppUser
             {
                 DisplayName = registerDto.DisplayName,
@@ -109,10 +115,10 @@ namespace API.Controllers
 
             var result = await _userManager.CreateAsync(user, registerDto.Password);
 
-            if (!result.Succeeded)
-            {
-                return BadRequest(new ApiResponse(400));
-            }
+            Console.WriteLine(result.Succeeded);
+            Console.WriteLine(result);
+            Console.WriteLine(user.ToString());
+            if (!result.Succeeded) return BadRequest(new ApiResponse(400));
 
             return new UserDto
             {
@@ -121,5 +127,6 @@ namespace API.Controllers
                 Email = user.Email
             };
         }
+
     }
 }
--- API/Dtos/AddressDto.cs
+++ API/Dtos/AddressDto.cs
@@ -1,11 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace API.Dtos
 {
     public class AddressDto
     {
+        [Required]
         public string FirstName { get; set; }
+        [Required]
         public string LastName { get; set; }
+        [Required]
         public string Street { get; set; }
+        [Required]
         public string Ward { get; set; }
+        [Required]
         public string District { get; set; }
         public string Zipcode { get; set; }
     }
