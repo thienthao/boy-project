2c64608b036303b771fa81911f5fcfaf55e9ecc8
thienthao <37302688+thienthao@users.noreply.github.com>
2020-07-15 04:32:39 +0700

    stripe setup, payment api, payment for client, payment component ui + handle

---
+++ API/Controllers/PaymentsController.cs
@@ -0,0 +1,29 @@
+using System.Threading.Tasks;
+using API.Errors;
+using Core.Entities;
+using Core.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class PaymentsController : ApiControllerBase
+    {
+        private readonly IPaymentService _paymentService;
+        public PaymentsController(IPaymentService paymentService)
+        {
+            _paymentService = paymentService;
+        }
+
+        [Authorize]
+        [HttpPost("{basketId}")]
+        public async Task<ActionResult<CustomerBasket>> CreateOrUpdatePaymentIntent(string basketId)
+        {
+            var basket = await _paymentService.CreateOrUpdatePaymentIntent(basketId);
+
+            if (basket == null) return BadRequest(new ApiResponse(400, "Problem with basket - in payments controller"));
+
+            return basket;
+        }
+    }
+}
--- client/src/app/checkout/checkout-review/checkout-review.component.ts
+++ client/src/app/checkout/checkout-review/checkout-review.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Observable } from 'rxjs';
+import { IBasket } from 'src/app/shared/models/basket';
+import { BasketService } from 'src/app/basket/basket.service';
+import { ToastrService } from 'ngx-toastr';
+import { CdkStepper } from '@angular/cdk/stepper';
 
 @Component({
   selector: 'app-checkout-review',
   templateUrl: './checkout-review.component.html',
-  styleUrls: ['./checkout-review.component.scss']
+  styleUrls: ['./checkout-review.component.scss'],
 })
 export class CheckoutReviewComponent implements OnInit {
+  @Input() appStepper: CdkStepper;
+  basket$: Observable<IBasket>;
 
-  constructor() { }
+  constructor(
+    private basketService: BasketService,
+    private toastr: ToastrService
+  ) {}
 
   ngOnInit(): void {
+    this.basket$ = this.basketService.basket$;
   }
 
+  createPaymentIntent() {
+    return this.basketService.createPaymentIntent().subscribe(
+      (response: any) => {
+        this.appStepper.next();
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
 }
